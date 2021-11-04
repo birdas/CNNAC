ce46f61708a680e724e698413e0d4f32772185de
Anthony Bird <birdas273@gmail.com>
2021-11-03 21:29:24 -0400

    maybe

--- testing.py
+++ testing.py
@@ -1,10 +1,12 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import os
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import load_img
 from IPython.display import Image, display
 from matplotlib import pyplot as plt
 from PIL import Image
@@ -14,7 +16,7 @@ img_width = 28
 img_height = 28
 # Our target layer: we will visualize the filters from this layer.
 # See `model.summary()` for list of layer names, if you want to change this.
-layer_name = "Conv2DT_1"
+layer_name = "test"
 
 def preprocess(array):
     """
@@ -67,7 +69,11 @@ def display1(array1, array2):
     plt.show()
 
 
+
 def compute_loss(input_image, filter_index, feature_extractor):
+    """
+    Computes the loss values for a specific image
+    """
     activation = feature_extractor(input_image)
     # We avoid border artifacts by only involving non-border pixels in the loss.
     filter_activation = activation[:, 2:-2, 2:-2, filter_index]
@@ -76,6 +82,9 @@ def compute_loss(input_image, filter_index, feature_extractor):
 
 @tf.function
 def gradient_ascent_step(img, filter_index, learning_rate, feature_extractor):
+    """
+    Performs a step of gradient ascent
+    """
     with tf.GradientTape() as tape:
         tape.watch(img)
         loss = compute_loss(img, filter_index, feature_extractor)
@@ -88,6 +97,9 @@ def gradient_ascent_step(img, filter_index, learning_rate, feature_extractor):
 
 
 def initialize_image():
+    """
+    Initializes an image with random noise
+    """
     # We start from a gray image with some random noise
     img = tf.random.uniform((1, img_width, img_height, 1))
     # ResNet50V2 expects inputs in the range [-1, +1].
@@ -96,6 +108,9 @@ def initialize_image():
 
 
 def visualize_filter(filter_index, feature_extractor):
+    """
+    Visualize filter a specific filter in a convolutional layer
+    """
     # We run gradient ascent for 20 steps
     iterations = 30
     learning_rate = 10.0
@@ -110,6 +125,9 @@ def visualize_filter(filter_index, feature_extractor):
 
 
 def deprocess_image(img):
+    """
+    Deprocess image
+    """
     # Normalize array: center on 0., ensure variance is 0.15
     img -= img.mean()
     img /= img.std() + 1e-5
@@ -128,11 +146,37 @@ def deprocess_image(img):
     return img
 
 
+def load_data(data_dir):
+    """
+    Load the raw image data into a data matrix, X, and target vector, y.
+
+    :param data_dir: The root directory of the data
+    :return: the data matrix 'x' and target vector 'y'
+    """
+    x = []
+    y = []
+    for file_name in os.listdir(data_dir + 'train/'):
+        image_file = os.path.join(data_dir + 'train/', file_name)
+        im = np.array(load_img(image_file))
+        im = tf.image.resize(im, size=(512, 512)).numpy()
+        im = tf.image.rgb_to_grayscale(im)
+        x.append(im)
+    for file_name in os.listdir(data_dir + 'test/'):
+        image_file = os.path.join(data_dir + 'test/', file_name)
+        im = np.array(load_img(image_file))
+        im = tf.image.resize(im, size=(512, 512)).numpy()
+        im = tf.image.rgb_to_grayscale(im)
+        y.append(im)
+    x = np.array(x)
+    y = np.array(y)
+    return x, y
 
 
 
 def main():
 
+    # Since we only need images from the dataset to encode and decode, we
+    # won't use the labels.
     # Since we only need images from the dataset to encode and decode, we
     # won't use the labels.
     (train_data, _), (test_data, _) = mnist.load_data()
@@ -145,25 +189,28 @@ def main():
     noisy_train_data = noise(train_data)
     noisy_test_data = noise(test_data)
 
+    # Display the train data and a version of it with added noise
+    display(train_data, noisy_train_data)
+
+
     input = layers.Input(shape=(28, 28, 1))
 
     # Encoder
-    x = layers.Conv2D(32, (3, 3), activation="relu", padding="same", name='Conv2D_1')(input)
+    x = layers.Conv2D(32, (3, 3), activation="relu", padding="same")(input)
     x = layers.MaxPooling2D((2, 2), padding="same")(x)
-    x = layers.Conv2D(32, (3, 3), activation="relu", padding="same", name='Conv2D_2')(x)
+    x = layers.Conv2D(32, (3, 3), activation="relu", padding="same", name='test')(x)
     x = layers.MaxPooling2D((2, 2), padding="same")(x)
-
-    # Can't go smaller or else we can't reconstruct back into the correct image size.
 
     # Decoder
-    x = layers.Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same", name='Conv2DT_1')(x)
-    x = layers.Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same", name='Conv2DT_2')(x)
-    x = layers.Conv2D(1, (3, 3), activation="sigmoid", padding="same", name='Conv2D_Out')(x)
+    x = layers.Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same")(x)
+    x = layers.Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same")(x)
+    x = layers.Conv2D(1, (3, 3), activation="sigmoid", padding="same")(x)
 
     # Autoencoder
     autoencoder = Model(input, x)
     autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
     autoencoder.summary()
+
 
     autoencoder.fit(
     x=train_data,
@@ -171,7 +218,7 @@ def main():
     epochs=1,
     batch_size=128,
     shuffle=True,
-    validation_data=(test_data, test_data),
+    validation_data=(test_data, test_data)
     )
 
     predictions = autoencoder.predict(test_data)
@@ -193,6 +240,7 @@ def main():
         data = np.zeros((h, w, 1), dtype=np.uint8)
         data[0:29, 0:29] = img
         plt.imshow(data, interpolation='nearest')
+        plt.savefig('fig' + str(len(all_imgs)) + '.png')
         plt.show()
 
         all_imgs.append(img)
