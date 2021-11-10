dc80a10b7ed4a17db212a15893af144a87f991fa
Anthony Bird <birdas273@gmail.com>
2021-11-10 13:32:56 -0500

    more filters

--- testing.py
+++ testing.py
@@ -163,15 +163,15 @@ def main():
     input = layers.Input(shape=(28, 28, 1))
 
     # Encoder
-    x = layers.Conv2D(32, (3, 3), activation="relu", padding="same", name='Conv2D_1')(input)
+    x = layers.Conv2D(32, (5, 5), activation="relu", padding="same", name='Conv2D_1')(input)
     x = layers.MaxPooling2D((2, 2), padding="same")(x)
-    x = layers.Conv2D(32, (3, 3), activation="relu", padding="same", name='Conv2D_2')(x)
+    x = layers.Conv2D(32, (5, 5), activation="relu", padding="same", name='Conv2D_2')(x)
     x = layers.MaxPooling2D((2, 2), padding="same")(x)
 
     # Decoder
-    x = layers.Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same", name='Conv2DT_1')(x)
-    x = layers.Conv2DTranspose(32, (3, 3), strides=2, activation="relu", padding="same", name='Conv2DT_2')(x)
-    x = layers.Conv2D(1, (3, 3), activation="sigmoid", padding="same", name='Conv2D_out')(x)
+    x = layers.Conv2DTranspose(32, (5, 5), strides=2, activation="relu", padding="same", name='Conv2DT_1')(x)
+    x = layers.Conv2DTranspose(32, (5, 5), strides=2, activation="relu", padding="same", name='Conv2DT_2')(x)
+    x = layers.Conv2D(1, (5, 5), activation="sigmoid", padding="same", name='Conv2D_out')(x)
 
     # Autoencoder
     autoencoder = Model(input, x)
@@ -264,7 +264,7 @@ def main():
         data = np.zeros((h, w, 1), dtype=np.uint8)
         data[0:29, 0:29] = img
         plt.imshow(data, interpolation='nearest')
-        plt.savefig('images/testing/activation_maps_3/' + str(i) + '.png')
+        plt.savefig('images/testing/activation_maps_5/' + str(i) + '.png')
         plt.show()
 
         i += 1
