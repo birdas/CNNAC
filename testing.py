a863c779838e8525a66f3bee42b6bbd589a115f2
birdas <birdas273@gmail.com>
2022-01-26 12:23:21 -0500

    removed rescaling

--- testing.py
+++ testing.py
@@ -24,7 +24,7 @@ def preprocess(array):
     Normalizes the supplied array and reshapes it into the appropriate format.
     """
 
-    array = array.astype("float32") / 255.0
+    #array = array.astype("float32") / 255.0
     array = np.reshape(array, (len(array), img_height, img_width, 1))
     return array
 
